1ef28d870d731d9fe8b84bc4fa6aa09b9437d0c2
tanvirahammed308 <imtanvir.email@gmail.com>
2023-07-22 18:50:11 +0600

    set footer

---
+++ src/Pages/Admission.jsx
@@ -0,0 +1,11 @@
+
+
+const Admission = () => {
+    return (
+        <div>
+           <h2>this is admission page</h2> 
+        </div>
+    );
+};
+
+export default Admission;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            this is Home page
+        </div>
+    );
+};
+
+export default Home;
